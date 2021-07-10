f2016ce6e41e15856bf69fdfb4a60b06659bc118
aboutmf <firmansyah@indoprima.id>
2021-07-11 00:43:09 +0700

    13. Not Found Route

--- src/routes/Router.js
+++ src/routes/Router.js
@@ -1,10 +1,11 @@
 import React from 'react'
-import { Route } from 'react-router-dom'
+import { Route, Switch } from 'react-router-dom'
 
 import Home from '../views/Home';
 import About from '../views/About';
 import Contact from '../views/Contact';
 import Detail from '../views/Detail';
+import NotFound from '../views/NotFound';
 
 /*
 * Home = '/'
@@ -15,10 +16,13 @@ import Detail from '../views/Detail';
 const Router = () => {
     return (
         <React.Fragment>
-            <Route path="/" exact component={Home} />
-            <Route path="/about" exact component={About} />
-            <Route path="/contact" exact component={Contact} />
-            <Route path="/detail/:id" exact component={Detail} />
+            <Switch>
+                <Route path="/" exact component={Home} />
+                <Route path="/about" exact component={About} />
+                <Route path="/contact" exact component={Contact} />
+                <Route path="/detail/:id" exact component={Detail} />
+                <Route component={NotFound} />
+            </Switch>
         </React.Fragment>
     )
 }
